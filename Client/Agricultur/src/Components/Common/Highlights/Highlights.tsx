7f13e2c40b80fcd0706b26520a7b45467994f95e
KiokoEric <kiokoerick040@gmail.com>
2025-04-21 20:43:28 +0300

    Creation of the reusable Highlights component in the client side of the Agricultur web application.

---
+++ Client/Agricultur/src/Components/Common/Highlights/Highlights.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface HighlightProps {
+    Icon: any;
+    Name: string;
+    children?: any;
+}
+
+const Highlights: React.FC<HighlightProps> = ({ Icon, Name, children }) => {
+return (
+    <figure className="flex gap-5 items-center p-4 w-52">
+        {Icon}
+        <figcaption>
+            <p className='text-lg'>{Name}</p>
+            {children}
+        </figcaption>
+    </figure>
+)
+}
+
+export default React.memo(Highlights)
