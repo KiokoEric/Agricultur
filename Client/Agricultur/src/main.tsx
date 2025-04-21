3efc7860b964af613b94bd2e20c00c0a86cc0be9
KiokoEric <kiokoerick040@gmail.com>
2025-04-21 17:04:56 +0300

    Creation of the Agricultur web application and installation of tailwind CSS to the application.

---
+++ Client/Agricultur/src/main.tsx
@@ -0,0 +1,18 @@
+import './index.css';
+import App from './App.tsx';
+import ReactDOM from 'react-dom/client';
+import { SnackbarProvider } from 'notistack';
+import { BrowserRouter } from "react-router-dom";
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core'; 
+
+// Add the imported icons to the library
+library.add(fas);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+<BrowserRouter>
+  <SnackbarProvider>
+      <App />
+  </SnackbarProvider>
+</BrowserRouter>
+)
