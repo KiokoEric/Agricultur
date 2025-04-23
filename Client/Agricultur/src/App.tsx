6ae6395ee46ef70996c465bb5495bb2366e40f90
KiokoEric <kiokoerick040@gmail.com>
2025-04-23 19:22:08 +0300

    Creation of the Header component in the client section of the Agricultur Web Application.

--- Client/Agricultur/src/App.tsx
+++ Client/Agricultur/src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css';
 import Weather from './Pages/Weather/Weather';
+import Header from './Components/Header/Header';
+import { Routes, Route } from 'react-router-dom';
+
 
 function App() {
 
   return (
     <div className='App' >
-      <Weather />
+      <Header />
+      <Routes>
+        <Route path='/Weather' element={<Weather />} />
+      </Routes>
     </div>
   )
 }
