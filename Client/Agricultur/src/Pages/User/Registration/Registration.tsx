4646306090807b145c032d949f42b9727dab29bb
“[KiokoEric]” <[kiokoerick040@gmail.com]>
2025-05-06 02:06:49 +0300

    Creation of the Login Page and introduction of AOS Library to the client side of the Agricultur web application.

--- Client/Agricultur/src/Pages/User/Registration/Registration.tsx
+++ Client/Agricultur/src/Pages/User/Registration/Registration.tsx
@@ -1,18 +1,25 @@
+import AOS from 'aos';
 import * as z from 'zod';
 import axios from "axios";
-import React from 'react';
 import { useSnackbar } from 'notistack';
+import React, { useEffect } from 'react';
+import { FaLeaf } from "react-icons/fa6";
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import Button from '../../../Components/Common/Button/Button';
+import RegistrationnImage from '../../../assets/Registration2.png';
+
+interface FormValues {
+    Name: string;
+    Email: string;
+    Password: string;
+};
 
 const Registration: React.FC = () => {
 
-    interface FormValues {
-        Name: string;
-        Email: string;
-        Password: string;
-    };
+    useEffect(() => {
+        AOS.init();
+    })
 
     // CREATION OF THE REGISTRATION ZOD SCHEMA
 
@@ -45,8 +52,15 @@ const Registration: React.FC = () => {
     }
 
 return (
-    <div>
-        <form method="post" onSubmit={handleSubmit(onRegistration)} encType="multipart/form-data" className='flex flex-col items-center gap-2'>
+    <div id='Registration' className='flex gap-10 items-center justify-center'>
+        <div data-aos="fade-right" data-aos-easing="linear" data-aos-duration="1500">
+            <img src={RegistrationnImage} alt="" />
+        </div>
+        <form method="post" onSubmit={handleSubmit(onRegistration)} encType="multipart/form-data" className='flex flex-col items-center gap-2' data-aos="fade-left" data-aos-easing="linear" data-aos-duration="1500">
+            <div className='my-10'>
+                <h2 className='flex text-5xl'>Welcome to Agri {<FaLeaf color="green" />} cultur</h2>
+                <p className='mt-10 text-xl text-center'>Create your account.</p>
+            </div>
             <div className='flex flex-col gap-2'>
                 <label className='font-bold' htmlFor="">Name</label> 
                 <input placeholder="Enter Name..." {...register('Name', { required: 'Name is required' })} className='border-black border-b h-8 outline-none truncate px-1 py-2 text-black w-80 sm:w-96' required />
@@ -64,7 +78,7 @@ return (
             </div>
             <Button
                 ButtonText='Sign Up'
-                ButtonStyle='bg-Blue cursor-pointer mt-1 text-center text-white px-3 py-1 rounded w-40 hover:bg-black'
+                ButtonStyle='bg-green-600 cursor-pointer flex items-center justify-center h-8 mt-1 text-center text-white rounded w-32 hover:bg-black'
                 onClick={handleSubmit(onRegistration)}
             />
         </form>
